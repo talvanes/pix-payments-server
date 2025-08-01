73e1ac267f9c31bea95d4536a6b09364c90f8ead
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2025-08-01 22:34:09 +0000

    test: set up testing support using jest

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+export default {
+    testEnvironment: 'node',
+    globals: {
+        'ts-jest': {
+            useESM: true,
+        },
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+    testMatch: ['**/tests/**/*.test.js'],
+    collectCoverageFrom: [
+        'db/**/*.js',
+        '!db/migrations/**/*.js', // Exclude individual migration files from coverage
+        '!**/node_modules/**',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
+}
